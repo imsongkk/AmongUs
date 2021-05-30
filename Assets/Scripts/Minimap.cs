7a187188bdf0123f73ccbd4590afe5748f9c9a2d
Imsung_You <isyou7487@naver.com>
2021-05-31 01:57:58 +0900

    using particle system, represented the Universe outside of map added vent anim, and pop-up minimap when user click the map button but still issue

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonClick()
+    {
+        print(target.activeSelf);
+        if (target.activeSelf)
+            target.SetActive(false);
+        else
+            target.SetActive(true);
+    }
+}
