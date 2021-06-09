a1dd659899e4dec59350b6d2f6b2abb8b928e9ab
Imsung_You <isyou7487@naver.com>
2021-06-09 15:32:42 +0900

    fixed TODO-26

---
+++ Assets/Scripts/IAnim.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IAnim
+{
+    void Play();
+}
+
+public class A : MonoBehaviour
+{
+
+}
+
+public class B : MonoBehaviour
+{
+
+}
+/*
+public class AnimVent : MonoBehaviour, IAnim
+{
+	public void Play()
+	{
+		print("A");
+	}
+}
+*/
